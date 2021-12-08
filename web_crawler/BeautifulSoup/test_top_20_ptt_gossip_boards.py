7aef0e7f56fd8ba440f9b836025a4b07878af8d4
Jesse <jessie97013@gmail.com>
2021-12-08 14:25:20 +0800

    test top_20_article_hyper_links success

--- web_crawler/BeautifulSoup/test_top_20_ptt_gossip_boards.py
+++ web_crawler/BeautifulSoup/test_top_20_ptt_gossip_boards.py
@@ -9,3 +9,12 @@ def response(url):
 
 def test_response():
     assert requests.codes.OK == response(url)
+
+
+def top_20_article_hyper_links(href):
+    # hyper_links = requests.get(href.get('href'))
+    return href.status_code
+
+
+def test_top_20_article_hyper_links():
+    assert requests.codes.OK == top_20_article_hyper_links(the_top_20_article_hyper_links)
--- web_crawler/BeautifulSoup/top_20_ptt_gossip_boards.py
+++ web_crawler/BeautifulSoup/top_20_ptt_gossip_boards.py
@@ -8,14 +8,17 @@ response = requests.get(url)
 
 Soup = BeautifulSoup(response.text, 'html.parser')
 
-# 作者
+the_top_20_article = Soup.select(".block-item > .block-item-title > a", limit=20)
+for i in the_top_20_article:
+    the_top_20_article_hyper_links = requests.get("https://pttgopolitics.com" + i.get('href'))
+# 作者(name)
 
-# 標題
+# 標題(thread-title)
 
-# 內文
+# 內文(article-metaline)
 
-# IP位置
+# IP位置(f2)
 
-# 日期
+# 日期(post-time)
 
-# 留言
+# 留言(push)

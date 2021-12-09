46ae7555176488aeb003bce546c053b5e7613652
Jesse <jessie97013@gmail.com>
2021-12-09 16:56:50 +0800

    字串處理完畢

--- web_crawler/BeautifulSoup/top_20_ptt_gossip_boards.py
+++ web_crawler/BeautifulSoup/top_20_ptt_gossip_boards.py
@@ -10,29 +10,25 @@ response = requests.get(url)
 
 Soup = BeautifulSoup(response.text, 'html.parser')
 
-all_article_links = Soup.select(".block-item > .block-item-title > a", limit=2)
+all_article_links = Soup.select(".block-item > .block-item-title > a", limit=20)
 the_top_20_ptt_gossip_boards_list = []
 for article_link in all_article_links:
 
     the_article_link = requests.get("https://pttgopolitics.com" + article_link.get('href'))
 
     soup = BeautifulSoup(the_article_link.text, "html.parser")
-    the_articles_data = {"author": "", "title": "", "context": "", "IP": "", "post time": "", "comments": ""}
-    the_comment = {"replay": "", "name": "", "comment": "", "IP": "", "time": ""}
+    the_articles_data = {"author_id": "", "title": "", "context": "", "author_IP": "", "post time": "", "comments": ""}
+    the_comment = {"reply": "", "user_id": "", "comment": "", "user_IP": "", "time": ""}
     context = ""
-    contexts = ""
-    text = ""
-    text_list = []
-    the_context = ""
     all_comments = []
-    all_replay = []
-    all_name = []
+    all_reply = []
+    all_user_name = []
     all_comment = []
     all_IP = []
     all_time = []
 
     # 作者(name)
-    the_articles_data["author"] = soup.find(class_='author').text.strip()
+    the_articles_data["author_id"] = soup.find(class_='author').text.strip()
 
     # 標題(thread-title)
     the_articles_data["title"] = soup.find(class_='thread-title').text.strip()
@@ -43,22 +39,23 @@ for article_link in all_article_links:
         the_articles_data["context"] = context
 
     # IP位置(f2)
-    the_articles_data["IP"] = ip_location(soup.find(class_='f2').text)
+    the_articles_data["author_IP"] = ip_location(soup.find(class_='f2').text)
 
     # 日期(post-time)
     the_articles_data["post time"] = soup.find(class_='post-time').text.strip()
 
 
     # 留言回應(push)
-    replays = soup.find_all('span', {'class': 'push-tag'})
-    for replay in replays:
-        all_replay.append(replay.text)
-    print(all_replay)
+    replys = soup.find_all('span', {'class': 'push-tag'})
+    for reply in replys:
+        # all_reply.append(reply.text)
+        all_reply.append(reply_reset(reply.text))
+    # print(all_reply)
 
     # 留言的人
-    names = soup.find_all('span', {'class': 'push-userid'})
-    for name in names:
-        all_name.append(name.text)
+    user_names = soup.find_all('span', {'class': 'push-userid'})
+    for user_id in user_names:
+        all_user_name.append(user_id.text)
 
     # 留言內文
     contents = soup.find_all('span', {'class': 'push-content'})
@@ -66,8 +63,8 @@ for article_link in all_article_links:
         all_comment.append(context_reset(content.text))
 
     # 留言人IP
-    IP = soup.find_all('span', {'class': 'push-ipdatetime'})
-    for ip in IP:
+    user_IP = soup.find_all('span', {'class': 'push-ipdatetime'})
+    for ip in user_IP:
         all_IP.append(comments_object_ip(ip.text))
 
     # 留言時間
@@ -77,10 +74,10 @@ for article_link in all_article_links:
 
     for i in range(len(all_time)):
         # the_comment={}
-        the_comment["replay"] = all_replay[i]
-        the_comment["name"] = all_name[i]
+        the_comment["reply"] = all_reply[i]
+        the_comment["user_id"] = all_user_name[i]
         the_comment["comment"] = all_comment[i]
-        the_comment["IP"] = all_IP[i]
+        the_comment["user_IP"] = all_IP[i]
         the_comment["time"] = all_time[i]
         all_comments.append(the_comment.copy())
     the_articles_data["comments"] = all_comments
